6a00bce4a662a43e39d7258a743c42eb68f992b2
Dmitry V. Alexeev <mail@alexeevdv.ru>
2017-02-03 00:17:17 +0300

    send and balance

--- src/Smsc.php
+++ src/Smsc.php
@@ -6,4 +6,38 @@ class Smsc extends \yii\base\Component
 {
     public $login;
     public $password;
+
+    public function send($numbers, $message)
+    {
+        $phones = $numbers;
+        if (is_array($numbers))
+        {
+            $phones = implode(";", $numbers);
+        }
+
+        $url = 'http://smsc.ru/sys/send.php?'.http_build_query([
+            'login' => $this->login,
+            'psw' => md5($this->password),
+            'phones' => $phones,
+            'mes' => $message,
+            'charset' => 'utf-8',
+            'fmt' => 3, // json
+            'cost' => 3, 
+        ]);
+
+        $response = file_get_contents($url);
+        return json_decode($response);
+    }
+
+    public function getBalance()
+    {
+        $url = 'http://smsc.ru/sys/balance.php?'.http_build_query([
+            'login' => $this->login,
+            'psw' => md5($this->password),
+            'fmt' => 3,
+        ]);
+
+        $response = file_get_contents($url);
+        return json_decode($response);
+    }
 }
